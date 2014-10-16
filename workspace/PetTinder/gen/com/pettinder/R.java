499433439681eb40cfe311592e5794e45481baf6
nmck257 <mckinneynicholas@gmail.com>
2014-10-15 20:23:21 -0400

    Mock-Up Discovery Functionality
    The layout isn't perfect, but the buttons do stuff and it's a start

--- workspace/PetTinder/gen/com/pettinder/R.java
+++ workspace/PetTinder/gen/com/pettinder/R.java
@@ -1077,9 +1077,16 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int blanket=0x7f020057;
+        public static final int forehead=0x7f020058;
+        public static final int ic_action_bad=0x7f020059;
+        public static final int ic_action_good=0x7f02005a;
+        public static final int ic_launcher=0x7f02005b;
+        public static final int loudnoises=0x7f02005c;
+        public static final int poofy=0x7f02005d;
     }
     public static final class id {
+        public static final int ProfileInfoLayout=0x7f05003e;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,15 +1098,25 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f05004a;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050046;
+        public static final int button2=0x7f050047;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
+        public static final int discoveryBreed=0x7f050040;
+        public static final int discoveryChoicesLayout=0x7f050041;
+        public static final int discoveryImage=0x7f05003d;
+        public static final int discoveryMore=0x7f050043;
+        public static final int discoveryName=0x7f05003f;
+        public static final int discoveryNo=0x7f050042;
+        public static final int discoveryProfileLayout=0x7f05003c;
+        public static final int discoveryYes=0x7f050044;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
@@ -1112,6 +1129,7 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int matches=0x7f050048;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1129,6 +1147,7 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
+        public static final int settings=0x7f050049;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
@@ -1136,6 +1155,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f050045;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1192,11 +1212,13 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_discovery=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int discovery_menu=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1241,7 +1263,17 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int button1_string=0x7f0a0010;
+        public static final int button2_string=0x7f0a0011;
+        public static final int discoveryBreed=0x7f0a0019;
+        public static final int discoveryImage=0x7f0a0017;
+        public static final int discoveryMore=0x7f0a0016;
+        public static final int discoveryName=0x7f0a0018;
+        public static final int discoveryNo=0x7f0a0014;
+        public static final int discoveryYes=0x7f0a0015;
         public static final int hello_world=0x7f0a000e;
+        public static final int matches_string=0x7f0a0013;
+        public static final int settings_string=0x7f0a0012;
     }
     public static final class style {
         /** 
--- workspace/PetTinder/src/com/pettinder/DiscoveryActivity.java
+++ workspace/PetTinder/src/com/pettinder/DiscoveryActivity.java
@@ -6,18 +6,80 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-
-
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ImageButton;
 
 public class DiscoveryActivity extends ActionBarActivity {
+private int profileNum = 0;
 
+	// Fetch the next discovery profile, updating the layout
+	private void getProfile() {
+		// This is a placeholder system for the sample profiles
+		// Fetch profile pic, name, breed
+		ImageView profilePic = (ImageView) findViewById(R.id.discoveryImage);
+		TextView name = (TextView) findViewById(R.id.discoveryName);
+		TextView breed = (TextView) findViewById(R.id.discoveryBreed);
+		switch (profileNum++ % 4) {
+				case 0:
+					profilePic.setImageResource(R.drawable.loudnoises);
+					name.setText("Spot");
+					breed.setText("Dogbreed");
+					break;
+				case 1:
+					profilePic.setImageResource(R.drawable.blanket);
+					name.setText("Pup");
+					breed.setText("Adorable");
+					break;
+				case 2:
+					profilePic.setImageResource(R.drawable.forehead);
+					name.setText("Billy");
+					breed.setText("Teddy Bear");
+					break;
+				case 3:
+					profilePic.setImageResource(R.drawable.poofy);
+					name.setText("Fluffy");
+					breed.setText("Something really fluffy");
+					break;
+				default:
+		}
+	}
 	
+	// Processes the user's selection for the current discovery profile
+	private void handleDiscoverySelection(boolean selection) {
+		if(selection) { // That is, user picked yes
+			// Handle yes from user
+		}
+		getProfile();
+	}
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discovery);
+        
+        // Obtain the first profile
+        getProfile();
+        
+        // Register buttons
+        ImageButton noButton = (ImageButton) findViewById(R.id.discoveryNo);
+        noButton.setOnClickListener(new ImageButton.OnClickListener() {
+        	public void onClick(View v) {
+        		handleDiscoverySelection(false);
+        	}
+        });
+        ImageButton yesButton = (ImageButton) findViewById(R.id.discoveryYes);
+        yesButton.setOnClickListener(new ImageButton.OnClickListener() {
+        	public void onClick(View v) {
+        		handleDiscoverySelection(true);
+        	}
+        });
+        ImageButton moreButton = (ImageButton) findViewById(R.id.discoveryMore);
+        moreButton.setOnClickListener(new ImageButton.OnClickListener() {
+        	public void onClick(View v) {
+        		// Fetch rest of profile (or whatever that button does)
+        	}
+        });
     }
 
     
@@ -71,6 +133,5 @@ public class DiscoveryActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    
     
 }
