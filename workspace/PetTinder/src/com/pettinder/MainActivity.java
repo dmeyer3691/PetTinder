9b8e54103b1e6fc36c1e64b1280443ec21e0a6e5
David Meyer <dmeyer3691@gmail.com>
2014-10-22 10:21:01 -0400

    Changed GPS stuff to not rely on google play services. It now works.

--- workspace/PetTinder/src/com/pettinder/MainActivity.java
+++ workspace/PetTinder/src/com/pettinder/MainActivity.java
@@ -1,233 +1,50 @@
 package com.pettinder;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.location.LocationClient;
+
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.DialogFragment;
-import android.app.Activity;
-import android.app.Dialog;
-//import android.app.DialogFragment;
+
+import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
+
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 
-public class MainActivity extends ActionBarActivity implements
-	GooglePlayServicesClient.ConnectionCallbacks,
-	GooglePlayServicesClient.OnConnectionFailedListener {
+public class MainActivity extends ActionBarActivity implements LocationListener{
 
 	
 	Button button1, button2;
     Intent discoveryIntent, settingsIntent;
-    LocationClient mLocationClient;
-    Location location;
-
-    // onClickListeners    
-	View.OnClickListener destroy = (new View.OnClickListener() {
-        public void onClick(View v) {
-            finish();
-        }
-    });
-	View.OnClickListener discovery = (new View.OnClickListener() {
-		public void onClick(View v){		
-			/*
-	    	if(mLocationClient.isConnected()){
-	    		//get location
-		    	Log.d("MainActivity", "Connected");
-	    		location = mLocationClient.getLastLocation();
-	    		Log.d("MainActivity", location.toString());
-	    	}
-	    	*/
-	    	Log.d("MainActivity", "Not Connected");
-			
-			startActivity(discoveryIntent);
-		}
-	});
-	
-	
-	
-	
-	
-	//
-	//
-	//Start stuff to allow location services 
-	//
-	//
-	
-	
-	   // Global constants
- /*
-  * Define a request code to send to Google Play services
-  * This code is returned in Activity.onActivityResult
-  */
- private final static int
-         CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-
- // Define a DialogFragment that displays the error dialog
- public static class ErrorDialogFragment extends DialogFragment {
-     // Global field to contain the error dialog
-     private Dialog mDialog;
-     // Default constructor. Sets the dialog field to null
-     public ErrorDialogFragment() {
-         super();
-         mDialog = null;
-     }
-     // Set the dialog to display
-     public void setDialog(Dialog dialog) {
-         mDialog = dialog;
-     }
-     // Return a Dialog to the DialogFragment.
-     @Override
-     public Dialog onCreateDialog(Bundle savedInstanceState) {
-         return mDialog;
-     }
- }
-
- /*
-  * Handle results returned to the FragmentActivity
-  * by Google Play services
-  */
- @Override
- protected void onActivityResult(
-         int requestCode, int resultCode, Intent data) {
-     // Decide what to do based on the original request code
-     switch (requestCode) {
-         case CONNECTION_FAILURE_RESOLUTION_REQUEST :
-         /*
-          * If the result code is Activity.RESULT_OK, try
-          * to connect again
-          */
-             switch (resultCode) {
-                 case Activity.RESULT_OK :
-                 /*
-                  * Try the request again
-                  */
-                 break;
-             }
-     }
-  }
- private boolean servicesConnected() {
-     // Check that Google Play services is available
-     int resultCode =
-             GooglePlayServicesUtil.
-                     isGooglePlayServicesAvailable(this);
-     // If Google Play services is available
-     if (ConnectionResult.SUCCESS == resultCode) {
-         // In debug mode, log the status
-         Log.d("Location Updates",
-                 "Google Play services is available.");
-         // Continue
-         return true;
-     // Google Play services was not available for some reason.
-     // resultCode holds the error code.
-     } else {
-         // Get the error dialog from Google Play services
-         Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
-                 resultCode,
-                 this,
-                 CONNECTION_FAILURE_RESOLUTION_REQUEST);
-
-         // If Google Play services can provide an error dialog
-         if (errorDialog != null) {
-             // Create a new DialogFragment for the error dialog
-             ErrorDialogFragment errorFragment =
-                     new ErrorDialogFragment();
-             // Set the dialog in the DialogFragment
-             errorFragment.setDialog(errorDialog);
-             // Show the error dialog in the DialogFragment
-             errorFragment.show(getSupportFragmentManager(), "Location Updates");
-
+    private TextView latituteField;
+    private TextView longitudeField;
+    private LocationManager locationManager;
+    private String provider;
+    
+ // onClickListeners    
+ 	View.OnClickListener destroy = (new View.OnClickListener() {
+         public void onClick(View v) {
+             finish();
          }
-         return false;
-     }
- }
-	
-	
-	/*
-     * Called by Location Services when the request to connect the
-     * client finishes successfully. At this point, you can
-     * request the current location or start periodic updates
-     */
-    @Override
-    public void onConnected(Bundle dataBundle) {
-        // Display the connection status
-        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-        
-        
-
-    }
-    /*
-     * Called by Location Services if the connection to the
-     * location client drops because of an error.
-     */
-    @Override
-    public void onDisconnected() {
-        // Display the connection status
-        Toast.makeText(this, "Disconnected. Please re-connect.",
-                Toast.LENGTH_SHORT).show();
-    }
-    /*
-     * Called by Location Services if the attempt to
-     * Location Services fails.
-     */
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        /*
-         * Google Play services can resolve some errors it detects.
-         * If the error has a resolution, try sending an Intent to
-         * start a Google Play services activity that can resolve
-         * error.
-         */
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(
-                        this,
-                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
-                /*
-                 * Thrown if Google Play services canceled the original
-                 * PendingIntent
-                 */
-            } catch (IntentSender.SendIntentException e) {
-                // Log the error
-                e.printStackTrace();
-            }
-        } else {
-            /*
-             * If no resolution is available, display a dialog to the
-             * user with the error.
-             */
-            //showErrorDialog(connectionResult.getErrorCode());
-        }
-    }
-	
-
-	//
-	//
-    //
-    //End stuff to make location services work
-    //
-    //
-	//
-	
-	
-	
-	
-	
-	
-	
-	
+     });
+ 	View.OnClickListener discovery = (new View.OnClickListener() {
+ 		public void onClick(View v){		 			
+ 			startActivity(discoveryIntent);
+ 		}
+ 	});
+ 	
+    
 	
 	
     @Override
@@ -238,14 +55,31 @@ public class MainActivity extends ActionBarActivity implements
         discoveryIntent = new Intent(this, DiscoveryActivity.class);
         settingsIntent = new Intent(this, SettingsActivity.class);
         
-        //location
-        mLocationClient = new LocationClient(this, this, this);
-
+        
     	// register buttons
     	button1 = (Button) findViewById(R.id.button1);
     	button1.setOnClickListener(destroy);
         button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(discovery);
+        latituteField = (TextView) findViewById(R.id.TextView02);
+        longitudeField = (TextView) findViewById(R.id.TextView04);
+
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+        	System.out.println("Provider " + provider + " has been selected.");
+        	onLocationChanged(location);
+        } else {
+        	latituteField.setText("Location not available");
+        	longitudeField.setText("Location not available");
+        }
         Log.d("Checkpoint 3", "Called onCreate");
     }
 
@@ -254,29 +88,7 @@ public class MainActivity extends ActionBarActivity implements
     protected void onStart(){
     	super.onStart();
     	
-    	//if google play services available
-    	if (servicesConnected()){
-    		   		
-    		// connect location client
-        	mLocationClient.connect();
-        	
-        	/*
-    		try {
-    			Thread.sleep(4000);
-    		} catch (InterruptedException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		}
-        	*/
-        	
-        	if(mLocationClient.isConnected()){
-        		//get location
-        		location = mLocationClient.getLastLocation();
-        		Log.d("MainActivity", location.toString());
-        	}
-        	Log.d("MainActivity", "Not Connected");
-    		
-    	}
+    	
 
         Log.d("Checkpoint 3", "Called onStart");
     }
@@ -290,27 +102,21 @@ public class MainActivity extends ActionBarActivity implements
     @Override
     protected void onResume(){
     	super.onResume();
-    	if(mLocationClient.isConnected()){
-    		//get location
-    		location = mLocationClient.getLastLocation();
-    		Log.d("MainActivity", location.toString());
-    	}
-    	Log.d("MainActivity", "Not Connected");
-    	
-    	
+    	locationManager.requestLocationUpdates(provider, 400, 1, this);
         Log.d("Checkpoint 3", "Called onResume");
     }
-    
+    	 
     @Override
     protected void onPause(){
     	super.onPause();
+    	locationManager.removeUpdates(this);
         Log.d("Checkpoint 3", "Called onPause");
     }
     
     @Override
     protected void onStop(){
+    	
     	super.onStop();
-        mLocationClient.disconnect();
 
         Log.d("Checkpoint 3", "Called onStop");
     }
@@ -340,7 +146,33 @@ public class MainActivity extends ActionBarActivity implements
         }
         return super.onOptionsItemSelected(item);
     }
-	
+
+    @Override
+    public void onLocationChanged(Location location) {
+      int lat = (int) (location.getLatitude());
+      int lng = (int) (location.getLongitude());
+      latituteField.setText(String.valueOf(lat));
+      longitudeField.setText(String.valueOf(lng));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+      // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+      Toast.makeText(this, "Enabled new provider " + provider,
+          Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+      Toast.makeText(this, "Disabled provider " + provider,
+          Toast.LENGTH_SHORT).show();
+    }
     
     
 }
